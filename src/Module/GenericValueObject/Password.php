b7a9f640c4a9b6c9051253c2e59d24d36aeb906a
Maik <froyi1986>
2017-12-14 22:43:37 +0100

    - sending password

--- src/Module/GenericValueObject/Password.php
+++ src/Module/GenericValueObject/Password.php
@@ -1,15 +1,54 @@
 <?php
 declare(strict_types=1);
 
-namespace JML\Module\GenericValueObject;
+namespace Project\Module\GenericValueObject;
 
-/**
- * @todo fill in the logic
- * Class Password
- * @package JML\Module\GenericValueObject
- */
 class Password
 {
+    protected $password;
 
-    /** preg_match für die Kontrolle groß-, kleinbuchstaben und Nummern muss mal ergoogelt werden*/
+    protected function __construct(string $password)
+    {
+        $this->password = $password;
+    }
+
+    public static function fromString(string $password): self
+    {
+        self::ensurePasswordIsValid($password);
+
+        return new self($password);
+    }
+
+    protected static function ensurePasswordIsValid(string $password): void
+    {
+        if (strlen($password) < 5) {
+            throw new \InvalidArgumentException('Dieser password ist zu kurz!', 1);
+        }
+
+        $uppercase = preg_match('@[A-Z]@', $password);
+        if ($uppercase === false) {
+            throw new \InvalidArgumentException('Dieses Passwort hat keine Großbuchstaben!', 1);
+        }
+
+        $lowercase = preg_match('@[a-z]@', $password);
+        if ($lowercase === false) {
+            throw new \InvalidArgumentException('Dieses Passwort enthält keinen Kleinbuchstaben!', 1);
+        }
+
+        $number = preg_match('@\d@', $password);
+        if($number === false) {
+            throw new \InvalidArgumentException('Dieses Passwort enthält keine Zahl!', 1);
+        }
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function __toString(): string
+    {
+        return $this->password;
+    }
 }
+
