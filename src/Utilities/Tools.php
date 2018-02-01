f7b731d3aba9702aa54b1a72027e4a4e54cbd091
maik.schoessler <ms2002@onlinehome.de>
2018-02-01 21:26:59 +0100

    tools

--- src/Utilities/Tools.php
+++ src/Utilities/Tools.php
@@ -5,13 +5,15 @@ namespace JML\Utilities;
 
 /**
  * Class Tools
- * @package JML\Utilities
+ * @package Project\Utilities
  */
 class Tools
 {
+    protected const STANDARD_URL = 'index.php';
+
     /**
      * @param string $name
-     * @return bool|string|int
+     * @return bool|string|int|array
      */
     public static function getValue(string $name)
     {
@@ -28,5 +30,58 @@ class Tools
         }
 
         return false;
+    }
+
+    /**
+     * @param string $name
+     * @return bool|array
+     */
+    public static function getFile(string $name)
+    {
+        if (isset($_FILES[$name]) && empty($_FILES[$name]) === false && $_FILES[$name]['error'] === 0) {
+            return $_FILES[$name];
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $route
+     * @return string
+     */
+    public static function getRouteUrl(string $route = '', array $parameter = []): string
+    {
+        if (empty($route)) {
+            return self::STANDARD_URL;
+        }
+
+        $url = self::STANDARD_URL . '?route=' . $route;
+
+        foreach ($parameter as $key => $value) {
+            $url .= '&' . $key . '=' . $value;
+        }
+
+        return $url;
+    }
+
+    /**
+     * @param string $text
+     * @param int    $amount
+     * @param bool   $points
+     * @return string
+     */
+    public static function shortener(string $text, int $amount = 50, bool $points = true): string
+    {
+        if (\strlen($text) <= $amount) {
+            return $text;
+        }
+
+        $newText = preg_replace("/[^ ]*$/", '', substr($text, 0, $amount));
+
+        if ($points === true) {
+            $newText .= ' ...';
+        }
+
+        return $newText;
     }
 }
